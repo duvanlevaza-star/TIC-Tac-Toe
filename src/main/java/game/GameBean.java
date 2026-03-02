ea6fc876d65b973bdcb59e75e0a574baaba6f55f
felipe <duvanlevaza@unicauca.edu.co>
2026-03-02 14:51:19 -0500

    modificacion 7*7

--- src/main/java/game/GameBean.java
+++ src/main/java/game/GameBean.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class GameBean implements Serializable {
 
-    private static final int GRID_SIZE = 3;
+    private static final int GRID_SIZE = 7;
 
     public enum GameState { NULL, O, X }
 
